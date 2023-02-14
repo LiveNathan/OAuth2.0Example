01f4353ee7c8b5cc8b3c3f7caf9f4802c1155e83
Nathan Lively <60303904+LiveNathan@users.noreply.github.com>
2023-02-14 12:01:22 -0600

    second commit

---
+++ src/test/java/com/Nathan/OAuth2/Example/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Nathan.OAuth2.Example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
