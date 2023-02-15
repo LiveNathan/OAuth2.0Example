96e3951dfeb8b0a5969379a26b117e2711b923fe
Nathan Lively <60303904+LiveNathan@users.noreply.github.com>
2023-02-14 19:41:04 -0600

    OAuth successful

---
+++ src/main/java/com/Nathan/OAuth2/Example/Controller/TheController.java
@@ -0,0 +1,21 @@
+package com.Nathan.OAuth2.Example.Controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TheController {
+    @GetMapping("/")
+    public String getIndexPage() {
+        return "index";
+    }
+
+    @GetMapping("/protected")
+    public String getProtectedPage(Authentication authentication) {
+        if (authentication != null) {
+            System.out.println(authentication);
+        }
+        return "index";
+    }
+}
