7472ff8109bd041e9590c62d55d6925962292eb4
Nathan Lively <60303904+LiveNathan@users.noreply.github.com>
2023-02-15 09:41:45 -0600

    removed private stuff

--- src/main/java/com/Nathan/OAuth2/Example/Security/SecurityConfig.java
+++ src/main/java/com/Nathan/OAuth2/Example/Security/SecurityConfig.java
@@ -12,7 +12,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/").permitAll()
+                .antMatchers("/", "/error").permitAll()
                 .antMatchers("/protected").authenticated()
                 .anyRequest().permitAll()
                 .and()
